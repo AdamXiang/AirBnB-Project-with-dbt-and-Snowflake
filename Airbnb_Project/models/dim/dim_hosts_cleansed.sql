f2f07727123ec4b18932c09730e2186c5f1dba30
Adam Chang <lavouver96@gmail.com>
2025-10-02 09:28:15 +0800

    fix syntax error and alias

--- Airbnb_Project/models/dim/dim_hosts_cleansed.sql
+++ Airbnb_Project/models/dim/dim_hosts_cleansed.sql
@@ -3,9 +3,9 @@ WITH src_hosts AS (
 )
 SELECT 
   host_id,
-  NVL(host_name, 'Anonymous')
+  NVL(host_name, 'Anonymous') AS host_name,
   is_superhost,
   created_at,
   updated_at
 FROM 
-src_hosts
+  src_hosts
